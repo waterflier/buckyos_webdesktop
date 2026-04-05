e31d2a3e68dea73de67bab116ee8e24634678b75
Liu Zhicong <waterflier@gmail.com>
2026-04-05 00:54:53 -0700

    fix stypes bug

--- src/app/homestation/HomeStationRoute.tsx
+++ src/app/homestation/HomeStationRoute.tsx
@@ -2,7 +2,7 @@ import { HomeStationView } from './HomeStationView'
 
 export function HomeStationRoute() {
   return (
-    <main className="min-h-dvh bg-[color:var(--cp-bg)] p-0 md:p-5">
+    <main className="homestation-root min-h-dvh bg-[color:var(--cp-bg)] p-0 md:p-5">
       <div
         className="mx-auto h-dvh w-full overflow-hidden md:h-[calc(100dvh-2.5rem)] md:max-w-[1600px] md:rounded-[28px] md:border md:shadow-[var(--cp-window-shadow)]"
         style={{
